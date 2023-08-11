3e46bb13cef889bf70e48fe9f748f82cecc65c28
kiyotaka <dreznuk23@gmail.com>
2023-08-11 19:12:27 +0300

    Imports, class

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import asyncio
+import discord
+from discord.ext import commands, tasks
+import os
+from dotenv import load_dotenv
+import youtube_dl  # open source code Download Manager for video and audio content from YouTube
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Download the video what we are sending to a port. It will download it and then it will play.
+load_dotenv()
+DISCORD_TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Загрузка аудио из видео файла
+
+youtube_dl.utils.bug_reports_message = lambda: ''  # lambda функция
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Класс - внутри класса переменная URL будет принимать URL-адреса. По URL-адресу будет загружено видео,
+# затем транслируется по порту как звук
 
+class YTDLSource(discord.PCMVolumeTransformer):  # Преобразователь громкости
+    def __init__(self, source, *, data, volume=0.5):  # ф-ия инициализации
+        super().__init__(source, volume)  # Источник звука и громкость звука
+        self.data = data
+        self.title = data.get('title')
+        self.url = ''
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Создадим метод, который будет являться методом класса
